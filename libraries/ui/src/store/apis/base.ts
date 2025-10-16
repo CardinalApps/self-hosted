434262c0dbade9a13cc078223238a431bc1092af
briiian <brian@cardinalapps.io>
2025-10-16 19:57:17 -0400

    feat: base api slice
    Signed-off-by: briiian <brian@cardinalapps.io>
    
    internal-repo-sha: d4ffd597ff384bf5ba75a6162d42a47882370f15

---
+++ libraries/ui/src/store/apis/base.ts
@@ -0,0 +1,12 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { HOME_SERVER_HOST } from "../../../env"
+import { prepareRTKQueryHeaders } from "../../lib/homeserver/prepareRTKQueryHeaders"
+
+export const baseApi = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${HOME_SERVER_HOST}/api/v1`,
+    prepareHeaders: prepareRTKQueryHeaders,
+  }),
+  endpoints: () => ({}),
+})
