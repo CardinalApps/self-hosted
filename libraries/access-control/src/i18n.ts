d969f7e1eeb04293c8bca36240db691c024634a8
briiian <brian@cardinalapps.io>
2025-12-30 20:48:18 -0500

    feat: add roles
    Signed-off-by: briiian <brian@cardinalapps.io>
    
    internal-repo-sha: 112829e1f378363ddabf5623de6ed676ad613d80

--- libraries/access-control/src/i18n.ts
+++ libraries/access-control/src/i18n.ts
@@ -91,8 +91,57 @@ export const ACi18n: StringList = {
   },
   "role.administrator.description": {
     "en": `
-      <p>The Administrator role is a powerful role with the same capabilities as the Owner role, but it can be assigned to multiple users.</p>
+      <p>The Administrator role is a powerful role with the same capabilities as the Owner role, but it can be assigned to multiple users, and does not trigger any Cardinal Cloud side effects.</p>
       <p>It is automatically assigned to the Guest Account, allowing users to access all of their server's features without needing to create a Cardinal account. It cannot be revoked from the Guest Account.</p>
+    `,
+  },
+
+  /**
+   * Media Apps User
+   */
+  "role.media_apps_user.name": {
+    "en": "Media Apps User",
+  },
+  "role.media_apps_user.description": {
+    "en": `
+      <p>The Media Apps User role grants access to all media apps, allowing the user to access the Music, Photos, and Cinema apps, and also future media applications.</p>
+      <p>It is the ideal role for someone that should have access to everything except the Administration app.</p>
+    `,
+  },
+
+  /**
+   * Music User
+   */
+  "role.music_user.name": {
+    "en": "Music User",
+  },
+  "role.music_user.description": {
+    "en": `
+      <p>The Music User role grants access exclusively to the Music app and all of its features.</p>
+    `,
+  },
+
+  /**
+   * Photos User
+   */
+  "role.photos_user.name": {
+    "en": "Photos User",
+  },
+  "role.photos_user.description": {
+    "en": `
+      <p>The Photos User role grants access exclusively to the Photos app and all of its features.</p>
+    `,
+  },
+
+  /**
+   * Cinema User
+   */
+  "role.cinema_user.name": {
+    "en": "Cinema User",
+  },
+  "role.cinema_user.description": {
+    "en": `
+      <p>The Cinema User role grants access exclusively to the Cinema app and all of its features.</p>
     `,
   },
 
